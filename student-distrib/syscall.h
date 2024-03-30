e3f0dde3c93fa386ec0938d0652deb6f05381cad
kellens4 <kellens4@illinois.edu>
2024-03-29 20:27:17 -0500

    kellen syscall start

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -41,5 +41,11 @@ typedef struct pcb {
     file_descriptor_t *fd_array[FD_ARRAY_SIZE];
 } pcb_t;
 
+int32_t sys_halt (uint8_t status);
+int32_t sys_execute (const uint8_t* command);
+int32_t sys_read (int32_t fd, void* buf, int32_t nbytes);
+int32_t sys_write (int32_t fd, const void* buf, int32_t nbytes);
+int32_t sys_open (const uint8_t* filename);
+int32_t sys_close (int32_t fd);
 
 #endif
