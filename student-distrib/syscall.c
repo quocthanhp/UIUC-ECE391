f560d1e0447a01bc173a4cc5d6a1756f9379dbc6
quocthanhp <phamquocthanh1502@gmail.com>
2024-04-01 13:19:03 -0500

    Add skeleton for execute

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -62,9 +62,7 @@ uint32_t load_program_image(const uint8_t *program_name) {
     dentry_t dentry;
     uint32_t inode;
     uint8_t *prog_buf;
-    uint8_t *magic_num;
     uint32_t prog_size;
-    uint8_t magic_num_size = 4;
     uint32_t prog_entry;
 
     /* Look up program in fs */ 
@@ -74,16 +72,6 @@ uint32_t load_program_image(const uint8_t *program_name) {
 
     inode = dentry.inode_num;
     prog_size = ((inode_t *) inode_start + inode)->len;
-
-    /* Check if program is an executable by reading the magic number (first 4 bytes) */ 
-    if (magic_num_size != read_data(inode, 0, magic_num, magic_num_size)) {
-        return -1;
-    }
-
-    if (magic_num[0] != MAGIC_0 || magic_num[1] != MAGIC_1 || magic_num[2] != MAGIC_2 || magic_num[3] != MAGIC_3) {
-        // Not an executable
-        return -1;
-    }
 
     /* Read program data */ 
     if (prog_size != read_data(inode, 0, prog_buf, prog_size)) {
@@ -105,8 +93,100 @@ uint32_t load_program_image(const uint8_t *program_name) {
     return prog_entry;
 }
 
+int8_t is_valid_file(const uint8_t *file_name) {
+    dentry_t dentry;
+    uint8_t *magic_num;
+    uint8_t magic_num_size = 4;
+
+    /* Look up program in fs */ 
+    if (read_dentry_by_name(file_name, &dentry) == -1) {
+        return - 1;
+    }
+
+    /* Check if program is an executable by reading the magic number (first 4 bytes) */ 
+    if (magic_num_size != read_data(dentry.inode_num, 0, magic_num, magic_num_size)) {
+        return -1;
+    }
+
+    if (magic_num[0] != MAGIC_0 || magic_num[1] != MAGIC_1 || magic_num[2] != MAGIC_2 || magic_num[3] != MAGIC_3) {
+        // Not an executable
+        return -1;
+    }
+
+    return 0;
+}
+
+uint8_t *parse(const uint8_t* command) {
+    if (command == NULL) {
+        return NULL;
+    }
+
+    uint8_t *buf;
+    uint8_t *program;
+    program = buf;
+    int inWord = 0;
+
+    while (*command != '\0') {
+        if (*command == ' ' && inWord) {
+            inWord = 0;
+            *buf = '\0'; // null-terminate word
+            break;
+        }
+        else if (*command != ' ') {
+            memcpy(buf, command, 1);
+            inWord = 1;
+            buf++;
+        }
+        command++;
+    }
+
+    return program;
+}
+
+int32_t execute(const uint8_t* command){
+    if (command == NULL) {
+        return -1;
+    }
+
+    /* Parse command to get file name of program */
+    uint8_t *program = parse(command);
+
+    /* Check file validity */
+    if (!is_valid_file(program)) {
+        return -1;
+    }
+
+    /* Get new pid */
+    uint32_t pid = get_next_pid();
+
+    if (pid < 0) {
+        return -1;
+    }
+
+    /* Set up paging */
+    set_program_page(pid);
+
+    /* Load file into memory */
+    load_program_image(program);
+
+    /* Create PCB */
+    pcb_t *prog_pcb;
+    if ((prog_pcb = get_next_pcb()) == NULL) {
+        return -1;
+    }
+
+    /* Prepare for context switch */
+
+    /* Push IRET context to kernel stack */
+
+    /* IRET */
+
+    /* return */
+}
+
+
+
 int32_t halt (uint8_t status){}
-int32_t execute (const uint8_t* command){}
 int32_t read (int32_t fd, void* buf, int32_t nbytes){}
 int32_t write (int32_t fd, const void* buf, int32_t nbytes){}
 int32_t open (const uint8_t* filename){}
